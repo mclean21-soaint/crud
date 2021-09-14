891479f49f467b4ede825c622800cfd287e0e768
Rolando Mclean <rmclean-consultor@innovacion.net>
2021-09-14 12:00:00 -0500

    comit init 14-9-2021

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+      
+        $responsearray=[];
+        return view('home',compact('responsearray'));
+    }
+
+    public function edit()
+    {
+        $search=request('validationCustom01');
+        $getstrg=('https://callcenter.innovacion.gob.pa/ws_pd/consulta_vac_persona.php?buscado=' . $search  .'&token=cf707bb196a16c156c9e79ef010&username=AIGGP233&password=7202e976031fcc7fa15927111');
+
+        $response = Http::get($getstrg);
+        
+        $responsearray=$response->json();
+
+        return view('home',compact('responsearray'));
+    }
+}
---
+++ database/factories/EarchFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\earch;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EarchFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = earch::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
